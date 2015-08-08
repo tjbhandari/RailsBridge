241e93bfb167c59ef85304f42ddc433b95e34454
Tushar <tjbhandari@gmail.com>
2015-08-08 15:31:23 -0700

    Added CSS and JS files

---
+++ resources/javascript.js
@@ -0,0 +1,25 @@
+function replaceName () {
+	var myNewName = window.prompt('Pick a new name!');
+	document.getElementById('user-name').innerHTML = myNewName;
+}
+
+function changeImage () {
+	var img = document.getElementById("main-image");
+	if (img.src.match("image1")) {
+		img.src = "image.jpg";
+	}
+	else {
+		img.src = "image1.jpg";
+	}	
+}
+
+$(document).ready(function() {
+  $("p").click(function(){ 
+  	// $(this).hide(); 
+   	$(this).css('color','purple'); 
+})
+});
+
+
+// document.onclick = replaceName;
+
